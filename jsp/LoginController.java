f485d0719458655128edfdaba61c8e92561676a2
LIVE_APP <35914192+LIVE-APP@users.noreply.github.com>
2019-05-13 19:43:55 +0530

    user_login, user_reg,Logout

---
+++ jsp/LoginController.java
@@ -0,0 +1,40 @@
+package com.cnsi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.cnsi.dao.Logindao;
+
+public class LoginController extends HttpServlet {
+	
+	public void doPost(HttpServletRequest req , HttpServletResponse res){
+		String s1 = req.getParameter("email");
+		String s2 = req.getParameter("pass");
+		//String s3 = req.getParameter("pass");
+		try{  
+			
+			HttpSession session = req.getSession();
+			session.setAttribute("email", s1);
+			
+			
+			System.out.println(s1);
+			Logindao dao = new Logindao();
+			
+			if(dao.service(s1,s2)){
+				res.sendRedirect("dashboard.jsp");
+			}else{
+				res.sendRedirect("index2.jsp");
+			}
+			
+			}catch(Exception e){ System.out.println(e);}  
+			}  
+	}
+
+
---
+++ jsp/Register.java
@@ -0,0 +1,53 @@
+package com.cnsi;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* Servlet implementation class LoginController
+*/
+public class Register extends HttpServlet {
+                
+                protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                                String bid=request.getParameter("name");
+                                String un=request.getParameter("phone_number");
+                                String pwd=request.getParameter("pswd");
+                                String email=request.getParameter("email");
+                     
+                                // Connect to mysql and verify username password
+                                
+                                try {
+                                                Class.forName("oracle.jdbc.driver.OracleDriver");
+                                // loads driver
+                                Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.86.25.49:1521:hcedev","tester","tester"); // gets a new connection
+                                PreparedStatement ps = c.prepareStatement("insert into user_reg values(?,?,?,?)");
+                                ps.setString(1, bid);
+                                ps.setString(2, un);
+                                ps.setString(3, pwd);
+                                ps.setString(4, email);
+                                ResultSet rs = ps.executeQuery();
+                                while(rs.next()) {
+                                      response.sendRedirect("index2.jsp");
+                                      return;
+                                }
+                                
+                                response.sendRedirect("error.html");
+                                return;
+                                
+                                } catch (ClassNotFoundException | SQLException e) {
+                                                // TODO Auto-generated catch block
+                                                e.printStackTrace();
+                                }
+                }
+
+}
+
