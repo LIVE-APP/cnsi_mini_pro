8011614064b2bac144f1da7d7580e5d0b0304b60
LIVE_APP <35914192+LIVE-APP@users.noreply.github.com>
2019-05-20 19:01:13 +0530

    Update OfficeDao.java

--- jsp/dao/OfficeDao.java
+++ jsp/dao/OfficeDao.java
@@ -10,17 +10,18 @@ public class OfficeDao {
 	
 		String url = "jdbc:oracle:thin:@10.86.25.49:1521/hcedev";
 		
-		String db_uname = "system";
-		String db_pass = "appu@12345";
+		String db_uname = "tester";
+		String db_pass = "tester";
 
 		public boolean o_service(String username, String mobile, String email, String source, String dest, String fromadd, String toadd,String Order_Code) throws SQLException {
 			System.out.println("oracle Passed url");
 			try {
 				Class.forName("oracle.jdbc.driver.OracleDriver");
 				
-				Connection con = DriverManager.getConnection(url,db_uname, db_pass);
+				Connection con = DriverManager.getConnection(url,db_uname,db_pass);
 				System.out.println("oracle Passed url");
-				String sql = "insert into office(name,mobile,email,source,destination,from_address,to_address,ordercode) values ('"+username+"','"+mobile+"','"+email+"','"+source+"','"+dest+"','"+fromadd+"','"+toadd+"','"+Order_Code+"')";
+				String sql = "insert into bookings(name,mobile,email,source,destination,from_address,to_address,ordercode) values ('"+username+"','"+mobile+"','"+email+"','"+source+"','"+dest+"','"+fromadd+"','"+toadd+"','"+Order_Code+"')";
+				System.out.println("sql string  Passed oracle Passed url");
 				PreparedStatement ps = con.prepareStatement(sql);
 /*				ps.setString(1,orderId);
 				ps.setString(2,name);
